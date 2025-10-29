a3f91930f734fae256f11a773d5dae1dea3ac14c
JoelWaring200 <000974820@mohawkcollege.ca>
2025-10-29 02:54:27 -0400

    made the class buttons and implemented it

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
+using System.Collections.Generic;
 using System.Numerics;
 
 // The namespace your code is in.
@@ -11,14 +12,35 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
+        //setup
+        int Width = 800;
+        int Height = 600;
 
+        //game states
+        bool titleScreen = true;
+        bool rulesScreen = false;
+        bool levelsScreen = false;
+        bool levelOne = false;
+        bool levelTwo = false;
+        bool levelThree = false;
+        //checking level locks
+        bool levelOneWon = false;
+        bool levelTwoWon = false;
+        //buttons
+        Button playButton = new Button(300, 400, 200, 50, "Play", 36, new Color(70, 130, 180, 255));
+        Button rulesButton = new Button(350, 500, 100, 50, "rules", 36, new Color(70, 130, 180, 255));
+        Button backButton = new Button(50, 50, 100, 50, "back", 38, new Color(120, 6, 6, 255));
+        Button levelOneButton = new Button(50, 250, 200, 200, "ONE", 50, new Color(70, 130, 180, 255));
+        Button levelTwoButton = new Button(300, 250, 200, 200, "TWO", 50, new Color(70, 130, 180, 255));
+        Button levelThreeButton = new Button(550, 250, 200, 200, "THREE", 50, new Color(70, 130, 180, 255));
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-
+            Window.SetSize(Width, Height);
+           
         }
 
         /// <summary>
@@ -26,8 +48,104 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            //mouse pos
+            Vector2 mousePos = Input.GetMousePosition();
+            //drawing the screen
+            Window.ClearBackground(Color.Black);
+            GameStateControl();
+            
+            
+        }
+        void GameStateControl()
+        {
+            Vector2 mousePos = Input.GetMousePosition();
+            if (titleScreen)
+            {
+                //Title
+                Text.Size = 70;
+                Text.Color = Color.White;
+                Text.Draw("Square Fighter", new Vector2(150, 150));
+                //buttons
+                playButton.drawButton(mousePos);
+                rulesButton.drawButton(mousePos);
+                //tracking button clicks
+                if (playButton.clicked(mousePos))
+                {
+                    levelsScreen = true;
+                    titleScreen = false;
+                }
+                if (rulesButton.clicked(mousePos)) 
+                {
+                    rulesScreen = true;
+                    titleScreen = false;
+                }
+            }
+            if (rulesScreen)
+            {
+                //Title
+                Text.Size = 70;
+                Text.Color = Color.White;
+                Text.Draw("Rules", new Vector2(150, 150));
+                //buttons
+                backButton.drawButton(mousePos);
+                
+                //tracking button clicks
+                if (backButton.clicked(mousePos))
+                {
+                    titleScreen = true;
+                    rulesScreen = false;
+                }
+                
+            }
+            if (levelsScreen)
+            {
+                //Title
+                Text.Size = 70;
+                Text.Color = Color.White;
+                Text.Draw("Levels", new Vector2(150, 150));
+                //buttons
+                backButton.drawButton(mousePos);
+                levelOneButton.drawButton(mousePos);
+                levelTwoButton.drawButton(mousePos);
+                levelThreeButton.drawButton(mousePos);
+                if (levelOneButton.clicked(mousePos))
+                {
+                    levelsScreen = false;
+                    levelOne = true;
+                }
+                if (levelTwoButton.clicked(mousePos) && levelOneWon == true)
+                {
+                    levelsScreen = false;
+                    levelTwo = true;
+                }
+                if (levelThreeButton.clicked(mousePos) && levelTwoWon == true)
+                {
+                    levelsScreen = false;
+                    levelThree = true;
+                }
+                //tracking button clicks
+                if (backButton.clicked(mousePos))
+                {
+                    titleScreen = true;
+                    levelsScreen = false;
+                }
+            }
+            if (levelOne)
+            {
+                // make the levels
+            }
+            if (levelTwo)
+            {
+
+            }
+            if (levelThree)
+            {
+
+            }
+
 
         }
     }
-
+    
 }
+    
---
+++ buttons.cs
@@ -0,0 +1,76 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+
+
+namespace MohawkGame2D
+{
+    public class Button
+    {
+        private int width;
+        private int height;
+        private int x;
+        private int y;
+        private string text;
+        private int fontSize;
+        private Color color;
+        private Color hoverColor;
+
+        public Button( int x, int y, int width, int height, string text, int fontSize, Color color)
+        {
+            this.width = width;
+            this.height = height;
+            this.x = x;
+            this.y = y;
+            this.text = text;
+            this.color = color;
+            this.fontSize = fontSize;
+
+            hoverColor = new Color(
+                (int)((255 + 2 * color.R) / 3),
+                (int) ((255 + 2 * color.G) / 3),
+                (int) ((255 + 2 * color.B) / 3),
+                255
+            );
+        }   
+
+        public void drawButton(Vector2 mousePos)
+        {
+            //check if players hovering over button
+            bool hovering = false;
+            if (mousePos.X > x && mousePos.X < x + width && mousePos.Y > y && mousePos.Y < y + height)
+            {
+                hovering = true;
+            }
+            //draws the buttons rectangle
+            if (hovering)
+            {
+                Draw.FillColor = hoverColor;
+                Draw.Rectangle(x, y, width, height);
+            }
+            else
+            {
+                Draw.FillColor = color;
+                Draw.Rectangle(x, y, width, height);
+            }
+            //draws the text centered
+            Text.Size = fontSize;
+            int textWidth = Raylib.MeasureText(text, fontSize);
+            int textX = x + (width - textWidth) / 2;
+            int textY = y + (height - fontSize) / 2;
+            Text.Color = Color.White;
+
+            Text.Draw(text, new Vector2(textX, textY));
+        }
+        public bool clicked(Vector2 mousePos)
+        {
+            bool hovering = false;
+            if (mousePos.X > x && mousePos.X < x + width && mousePos.Y > y && mousePos.Y < y + height)
+            {
+                hovering = true;
+            }
+            bool isMousePressed = Input.IsMouseButtonPressed(0);
+            return hovering && isMousePressed;
+        }
+    }
+}
